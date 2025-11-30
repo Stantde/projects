3ba178ae016153523898862221e4a6af3a5b69b9
Spectre050287 <d.stanton256@gmail.com>
2025-11-30 10:43:01 -0600

    new file:   python/Adventures_with_python/current_version/shabby_abbey.py 	deleted:    python/Adventures_with_python/current_version/shabby_abbeyv1.py 	new file:   python/Adventures_with_python/version_01/shabby_abbeyv1.py

---
+++ python/Adventures_with_python/current_version/shabby_abbey.py
@@ -0,0 +1,15 @@
+"""
+    Shabby Abbey v2: 
+    Obtain a command from the player and manipulate it.
+    DS
+    11-30-2025
+"""
+# Annotate variable
+command: str
+
+# Greet player.
+print("Welcome, Adventurer!")
+
+# Obtain the player's command and manipulate it.
+command = input("What is your command? ")
+print(f"Your command is {command}.")
