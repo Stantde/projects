043678e3eb2b47bcb3f854c04d2204ef18472d49
Spectre050287 <d.stanton256@gmail.com>
2025-11-30 11:26:42 -0600

    Gotta take a break.

--- python/Adventures_with_python/current_version/shabby_abbey.py
+++ python/Adventures_with_python/current_version/shabby_abbey.py
@@ -13,3 +13,13 @@ print("Welcome, Adventurer!")
 # Obtain the player's command and manipulate it.
 command = input("What is your command? ")
 print(f"Your command is {command}.")
+
+'''
+match expression:
+    case pattern1:
+        # code block for pattern1
+    case pattern2:
+        # code block for pattern2
+    case _:  # default case
+        # code block for no match
+'''
